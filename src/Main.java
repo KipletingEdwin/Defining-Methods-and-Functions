bec834e25e88adba93574a9c50f1b68a03d7b816
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-18 14:42:56 +0300

    defined methods and functions

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+//        helloWorld();
+        int c = sum(5, 6);
+        System.out.println(c);
+
+
 
     }
+
+    public  static void helloWorld(){
+        for(int i = 0; i< 10; i++){
+            System.out.println("Hello World");
+        }
+    }
+
+    public  static  int sum(int a, int b){
+        return  a + b;
+
+    }
+
+
 }
